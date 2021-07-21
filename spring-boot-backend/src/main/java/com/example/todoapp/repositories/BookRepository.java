1e4487eb196e42a2e1a617b43e1d3ae15e8f630a
Utkarsh Maken <35842080+utkarshmaken@users.noreply.github.com>
2021-07-21 16:28:42 +0530

    Update BookRepository.java

--- spring-boot-backend/src/main/java/com/example/todoapp/repositories/BookRepository.java
+++ spring-boot-backend/src/main/java/com/example/todoapp/repositories/BookRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface BookRepository extends MongoRepository<Book, String> {
 	Book findByTitle(String title);
-    List<Book> findByAuthor(String author);
-    List<Book> findByCity(String city);
+	List<Book> findByAuthor(String author);
+	List<Book> findByCity(String city);
 }
